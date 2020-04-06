44fd94c06c2e076b3e40cd7843da4acaa252743e
Ajay Raj <ajay@MacBook-Pro-117.local>
2020-04-06 15:52:30 -0700

    changed order of functions

--- utils.py
+++ utils.py
@@ -14,6 +14,25 @@ def is_valid_network(G, T):
 
     return nx.is_tree(T) and nx.is_dominating_set(G, T.nodes)
 
+def average_pairwise_distance(T):
+    """
+    Computes the average pairwise distance between vertices in T.
+    This is what we want to minimize!
+
+    Note that this function is a little naive, i.e. there are much
+    faster ways to compute the average pairwise distance in a tree. 
+    Feel free to write your own!
+
+    Args:
+        T: networkx.Graph, a tree
+
+    Returns:
+        double: the average pairwise distance
+    """
+    path_lengths = nx.all_pairs_dijkstra_path_length(T)
+    total_pairwise_distance = sum([sum(length[1].values()) for length in path_lengths]) / 2
+    return total_pairwise_distance / (len(T) * (len(T) - 1))
+
 def average_pairwise_distance_fast(T):
     """Calculates the average pairwise distance for a tree in linear time.
 
@@ -66,22 +85,3 @@ def average_pairwise_distance_fast(T):
         w = T[c][p]['weight']
         cost += (a * b * w)
     return cost / (len(T) * (len(T) - 1))
-
-def average_pairwise_distance(T):
-    """
-    Computes the average pairwise distance between vertices in T.
-    This is what we want to minimize!
-
-    Note that this function is a little naive, i.e. there are much
-    faster ways to compute the average pairwise distance in a tree. 
-    Feel free to write your own!
-
-    Args:
-        T: networkx.Graph, a tree
-
-    Returns:
-        double: the average pairwise distance
-    """
-    path_lengths = nx.all_pairs_dijkstra_path_length(T)
-    total_pairwise_distance = sum([sum(length[1].values()) for length in path_lengths]) / 2
-    return total_pairwise_distance / (len(T) * (len(T) - 1))
