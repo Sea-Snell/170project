3b980aeaff343eeaa1f2a598af0b6d7ed0183f9c
Ajay Raj <ajay@MacBook-Pro-117.local>
2020-04-21 23:57:40 -0700

    added 1 vertex + connected check to average_pairwise_slow

--- utils.py
+++ utils.py
@@ -31,6 +31,11 @@ def average_pairwise_distance(T):
     Returns:
         double: the average pairwise distance
     """
+    if not nx.is_connected(T):
+        raise ValueError("Tree must be connected")
+
+    if len(T) == 1: return 0
+    
     path_lengths = nx.all_pairs_dijkstra_path_length(T)
     total_pairwise_distance = sum([sum(length[1].values()) for length in path_lengths])
     return total_pairwise_distance / (len(T) * (len(T) - 1))
